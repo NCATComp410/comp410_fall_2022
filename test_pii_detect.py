55e9116fbd2da71486f543201098b3346f094724
claesmk <claesmk@gmail.com>
2022-08-31 19:47:27 -0400

    Create a unit-test failure

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -4,7 +4,12 @@ from pii_detect import show_aggie_pride
 
 class Comp410TestCase(unittest.TestCase):
     def test_show_aggie_pride(self):
-        self.assertEqual(show_aggie_pride(), ['Aggie Pride - Worldwide'])
+        # show_aggie_pride() returns a list of slogans
+        result_list = show_aggie_pride()
+
+        # make sure each slogan is in the expected position
+        # merge errors are a common reason for failures
+        self.assertEqual(result_list[0], 'Aggie Pride - WW')
 
 
 if __name__ == '__main__':
