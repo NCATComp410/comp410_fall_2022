fadb73b8e6143386b861b0182689523d6192b2ff
claesmk <claesmk@gmail.com>
2022-09-01 16:33:35 -0400

    Added aggies do

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -19,13 +19,19 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(1, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(2, len(result_list), 'Unexpected number of slogans')
 
     def test_ap_ww(self):
         # make sure each slogan is in the expected position
         # merge errors are a common reason for failures
         result_list = show_aggie_pride()
         self.assertEqual(result_list[0], 'Aggie Pride - Worldwide')
+
+    def test_aggie_do(self):
+        # make sure each slogan is in the expected position
+        # merge errors are a common reason for failures
+        result_list = show_aggie_pride()
+        self.assertEqual(result_list[1], 'Aggies Do!')
 
 
 if __name__ == '__main__':
