218ca759df38facf2bde8b8126b1ecf7b78de4dc
Nick Chowa <nchowa593@gmail.com>
2022-09-14 12:24:36 -0400

    update pii_detect
    just updating to the current branch

--- pii_detect.py
+++ pii_detect.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-def show_aggie_pride():
-    slogan_list = ['Aggie Pride - Worldwide', 'Aggies DO']
-=======
 def show_aggie_pride() -> list:
     """Returns a list of favorite aggie slogans"""
     # python lists
@@ -14,7 +10,6 @@ def show_aggie_pride() -> list:
                    'Thats What Aggies Do!',
                    'Lets Go Aggies!',
                    'Aggies skate, Aggies grind!',]
->>>>>>> 442beeb9207e8d66a6ff75fa8378c0d7b4c3a553
 
     return slogan_list
 
