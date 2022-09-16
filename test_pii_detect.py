72b339b3dad4c4ec904543efd901c386b92e1557
DougGajda <112100715+DougGajda@users.noreply.github.com>
2022-09-16 13:34:29 -0400

    Re done slogan into updated list

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -20,7 +20,7 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(11, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(12, len(result_list), 'Unexpected number of slogans')
 
     def test_starts_with_test(self):
         # In order to run as a test case the method name must start with test
@@ -93,7 +93,6 @@ class Comp410TestCase(unittest.TestCase):
         # merge errors are a common reason for failures
         result_list = show_aggie_pride()
         self.assertEqual(result_list[9], 'Aggie born, Aggie bred')
-<<<<<<< Updated upstream
 
     def test_aggies_stick(self):
         # make sure each slogan is in the expected position
@@ -101,12 +100,10 @@ class Comp410TestCase(unittest.TestCase):
         result_list = show_aggie_pride()
         self.assertEqual(result_list[10], 'Aggies stick together')
         
-=======
     def test_NeverUnderestimate_MoveForward(self):
         # make sure each slogan is in the expected position
         # merge errors are a common reason for failures
         result_list = show_aggie_pride()
-        self.assertEqual(result_list[10], 'Never Ever Underestimate An Aggie. Move Forward With Purpose.')
->>>>>>> Stashed changes
+        self.assertEqual(result_list[11], 'Never Ever Underestimate An Aggie. Move Forward With Purpose.')
 if __name__ == '__main__':
     unittest.main()
