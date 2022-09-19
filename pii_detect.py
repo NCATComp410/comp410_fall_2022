8befdd74196e33e6f74a587bbe3becbacc2f909c
Toinje' Maxson <54955193+Tmmaxson@users.noreply.github.com>
2022-09-19 18:47:38 -0400

    Update pii_detect.py

--- pii_detect.py
+++ pii_detect.py
@@ -9,13 +9,13 @@ def show_aggie_pride() -> list:
                    'Thats on 1891!',
                    'Thats What Aggies Do!',
                    'Lets Go Aggies!',
-                   'Aggies skate, Aggies grind!'
-                   'Go Aggie Pride!'     
+                   'Aggies skate, Aggies grind!'     
                    'Show em what Aggies do',
                    'Aggie born, Aggie bred',
                    'Aggies stick together',
                    'Never Ever Underestimate An Aggie. Move Forward With Purpose.',
-                   'Aggie For Life!']
+                   'Aggie For Life!',
+                   'Go Aggie Pride!']
 
     return slogan_list
 
