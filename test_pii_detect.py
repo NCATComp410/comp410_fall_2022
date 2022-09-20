7916fe38ed443bd05bab08d13e5f4323d4e8dae2
Ericas14 <52893140+Ericas14@users.noreply.github.com>
2022-09-20 14:41:16 -0400

    Add Aggies Slogan

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -20,7 +20,7 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(16, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(17, len(result_list), 'Unexpected number of slogans')
 
     def test_starts_with_test(self):
         # In order to run as a test case the method name must start with test
@@ -129,6 +129,12 @@ class Comp410TestCase(unittest.TestCase):
         # merge errors are a common reason for failures
         result_list = show_aggie_pride()
         self.assertEqual(result_list[15], 'Aggies Achieve!')
+
+    def test_Aggie_Achieve(self):
+        # make sure each slogan is in the expected position
+        # merge errors are a common reason for failures
+        result_list = show_aggie_pride()
+        self.assertEqual(result_list[16], 'Aggies Go!')
 
         
 if __name__ == '__main__':
