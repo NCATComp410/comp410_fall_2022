0bf8b4a87f63b2dfa253b7863e669b609796a5ba
kalynmatth <54991933+kalynmatth@users.noreply.github.com>
2022-09-19 22:40:19 -0400

    Update test_pii_detect.py

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -122,7 +122,7 @@ class Comp410TestCase(unittest.TestCase):
         # make sure each slogan is in the expected position
         # merge errors are a common reason for failures
         result_list = show_aggie_pride()
-        self.assertEqual(result_list[14], 'Aggie Nation!')
+        self.assertEqual(result_list[15], 'Aggie Nation!')
 
         
 if __name__ == '__main__':
