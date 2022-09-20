e85563c389004f6cfc2f52df48c1ce2ed60d30ba
ClarenceRose <59844708+ClarenceRose@users.noreply.github.com>
2022-09-20 17:38:58 -0400

    Update pii_detect.py

--- pii_detect.py
+++ pii_detect.py
@@ -2,8 +2,7 @@ def show_aggie_pride() -> list:
     """Returns a list of favorite aggie slogans"""
     # python lists
     # https://developers.google.com/edu/python/lists
-    slogan_list = ['Aggie Pride - Worldwide']
-
+    slogan_list = ['Aggie Pride - Worldwide'
                    'Aggies Do!',
                    'Go Aggies',
                    'We are Aggies!',
