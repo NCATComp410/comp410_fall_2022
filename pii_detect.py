2ce6de2ac3938c0df683e855f7927ed25cd64fe2
Brianna Frost <55293787+FrostBrianna@users.noreply.github.com>
2022-09-20 11:26:14 -0400

    Update pii_detect.py

--- pii_detect.py
+++ pii_detect.py
@@ -17,7 +17,8 @@ def show_aggie_pride() -> list:
                    'Aggie For Life!',
                    'Go Aggie Pride!', 
                    'Aggie Nation!',
-                   'Aggies Achieve!']
+                   'Aggies Achieve!',
+                   'Aggies Learn']
 
     return slogan_list
 
