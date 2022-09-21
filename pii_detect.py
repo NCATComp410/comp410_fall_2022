9d21eb6b6d8d308f84cbe78e8e79c5d1e64157b4
ClarenceRose <59844708+ClarenceRose@users.noreply.github.com>
2022-09-21 13:13:50 -0400

    Update pii_detect.py

--- pii_detect.py
+++ pii_detect.py
@@ -18,7 +18,7 @@ def show_aggie_pride() -> list:
                    'Aggie For Life!',
                    'Go Aggie Pride!', 
                    'Aggie Nation!',
-                   'Aggies Achieve!',
+                   'Aggies Achieve!'
                    'A G G I E what!',
                    'Aggie what? Pride what give me that, give me that']
 
