49ba6c37c20742cee3e701a7b65ead4d3a864e38
ClarenceRose <59844708+ClarenceRose@users.noreply.github.com>
2022-09-21 13:55:05 -0400

    Update test_pii_detect.py

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -20,7 +20,7 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(18, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(19, len(result_list), 'Unexpected number of slogans')
 
     def test_starts_with_test(self):
         # In order to run as a test case the method name must start with test
@@ -110,11 +110,9 @@ class Comp410TestCase(unittest.TestCase):
         result_list = show_aggie_pride()
         self.assertEqual(result_list[12], 'Aggie For Life!')
         
-        
     def test_new_aggie_slogan(self):
        result_list = show_aggie_pride()
        self.assertEqual(result_list[13], 'Go Aggie Pride!')
-
     
     def test_Aggie_National(self):
         # make sure each slogan is in the expected position
@@ -134,7 +132,7 @@ class Comp410TestCase(unittest.TestCase):
         
     def test_aggie_what_pride_what(self):
         result_list = show_aggie_pride()
-        self.assertEqual(result_list[18], 'Aggie what? Pride what give me that, give me that')
+        self.assertEqual(result_list[18], 'A G G I E what!')
 
         
 if __name__ == '__main__':
