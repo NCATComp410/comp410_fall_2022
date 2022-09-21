5b34aeafddc46bd93d21257a8881a1d2d12f8e09
Kennedy Marsh <kikimarsholl@gmail.com>
2022-09-21 14:28:10 -0400

    Updated slogan list

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -20,7 +20,7 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(18, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(19, len(result_list), 'Unexpected number of slogans')
 
     def test_starts_with_test(self):
         # In order to run as a test case the method name must start with test
@@ -140,7 +140,11 @@ class Comp410TestCase(unittest.TestCase):
     def test_aggie_what_pride_what(self):
         result_list = show_aggie_pride()
         self.assertEqual(result_list[17], 'Aggie what? Pride what give me that, give me that')
-        
+
+    def test_aggie_what_pride_what(self):
+        result_list = show_aggie_pride()
+        self.assertEqual(result_list[18], 'Aggies Rule, Eagles Drool')
+
 
         
 if __name__ == '__main__':
