08ccd4689290a39514a3a6a174cbc3239cd61de0
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2022-09-21 08:13:42 -0400

    Update test_pii_detect.py

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -118,5 +118,9 @@ class Comp410TestCase(unittest.TestCase):
         result_list = show_aggie_pride()
         self.assertEqual(result_list[13], 'Aggies think, Aggies do!')
 
+    def test_aggie_pride(self):
+        resultOlist = show_aggie_pride()
+        self.assertEqual(result_list[15] == 'Can I Get an Aggie Pride?')
+
 if __name__ == '__main__':
     unittest.main()
