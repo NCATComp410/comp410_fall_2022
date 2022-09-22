ed7f9f53da4f727f63c41642fd5e70fa6b26ba6e
Brianna Frost <55293787+FrostBrianna@users.noreply.github.com>
2022-09-21 22:18:44 -0400

    Update test_pii_detect.py

--- test_pii_detect.py
+++ test_pii_detect.py
@@ -20,7 +20,7 @@ class Comp410TestCase(unittest.TestCase):
         self.assertTrue(has_aggie, 'No Aggie slogans found')
 
         # make sure the list has the expected number of slogans
-        self.assertEqual(25, len(result_list), 'Unexpected number of slogans')
+        self.assertEqual(24, len(result_list), 'Unexpected number of slogans')
 
 
     def test_starts_with_test(self):
