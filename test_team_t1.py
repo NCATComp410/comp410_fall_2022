7e1df92361a1b113642e6206cfb7014053e925e8
Igconze <59844877+Igconze@users.noreply.github.com>
2022-10-17 20:36:13 -0400

    added support for us ssn

--- test_team_t1.py
+++ test_team_t1.py
@@ -36,7 +36,36 @@ class TeamFrostTests(unittest.TestCase):
 
     def test_find_us_ssn(self):
         results_list = find_us_ssn('My social security number is 123-45-6789')
-        self.assertEqual(results_list, [])
+        self.assertEqual(results_list[0], '123-45-6789')
+        # tests with a SSN at the end of the string
+        results_list = find_us_ssn('My social security number is 123-45-6789')
+        self.assertEqual(results_list[0], '123-45-6789')
+
+        # tests with a SSN at the beginning of a string
+        results_list = find_us_ssn('123-45-6789 is my social security number')
+        self.assertEqual(results_list[0], '123-45-6789')
+
+        # tests with a SSN in the middle of a string
+        results_list = find_us_ssn('Can the number 123-45-6789 be my new SSN')
+        self.assertEqual(results_list[0], '123-45-6789')
+
+        # tests with a different number for SSN
+        results_list = find_us_ssn('My social security number is 012-01-0123')
+        self.assertEqual(results_list[0], '012-01-0123')
+
+        # tests with multiple SSN in the string
+        results_list = find_us_ssn('Is my SSN 123-45-6789 or is 123-12-1234 my SSN')
+        self.assertEqual(results_list[0], '123-45-6789')
+        self.assertEqual(results_list[1], '123-12-1234')
+
+        # tests SSN without dashes
+        results_list = find_us_ssn('My SSN is 123456789')
+        # US SSNs must have dashes after the third digit and after the 5th digit
+        self.assertFalse(results_list)
+
+        # tests with letters entered instead of digits
+        results_list = find_us_ssn('my SSN is 123-four5-6789')
+        self.assertFalse(results_list)
 
     def test_find_email(self):
         results_list = find_email('My email address is jim.jones@jones.com')
