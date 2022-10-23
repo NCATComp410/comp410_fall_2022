65a5595dbaea726fc7bb56ed5d4794303b428642
ClarenceRose <59844708+ClarenceRose@users.noreply.github.com>
2022-10-23 13:19:44 -0400

    Update test_team_sei.py

--- test_team_sei.py
+++ test_team_sei.py
@@ -4,8 +4,26 @@ from pii_team_sei import *
 
 class TeamFrostTests(unittest.TestCase):
     def test_find_us_phone_number(self):
+        
+        #test at the beginning of a string
+        results_list = find_us_phone_number('123-456-7890 is my phone number')
+        self.assertEqual(results_list[0], '123-456-7890')
+
+        #test in the middle of a string
+        results_list = find_us_phone_number('You can reach me at 123-456-7890. That is my number')
+        self.assertEqual(results_list[0], '123-456-7890')
+            
+        #test multiple phone numbers
+        results_list = find_us_phone_number('123-456-7890 is my phone number. Her number is 987-654-3210')
+        self.assertEqual(results_list[0], '123-456-7890')
+        self.assertEqual(results_list[1], '987-654-3210')
+        
+        #test at the end of a string
         results_list = find_us_phone_number('My phone number is 123-456-7890')
-        self.assertEqual(results_list, [])  # add assertion here
+        self.assertEqual(results_list[0], '123-456-7890')
+        
+        #result_list should be empty
+        self.assertFalse(results_list) 
 
     def test_find_visa_mastercard(self):
         results_list = find_visa_mastercard('My credit card number is 1234-5678-9012-3456')
