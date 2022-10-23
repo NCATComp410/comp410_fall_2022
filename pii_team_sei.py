9dd08886ce1595722ae173743ca6926554e0a27d
davismiah04 <59848795+davismiah04@users.noreply.github.com>
2022-10-23 13:00:47 -0400

    Update pii_team_sei.py

--- pii_team_sei.py
+++ pii_team_sei.py
@@ -21,8 +21,9 @@ def find_amex(text) -> list:
 
 def find_us_ssn(text) -> list:
     """Finds all occurrences of a US social security number in a text string"""
-    # match a 9 digit social security number
     return []
+    regex = "^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$";
+
 
 
 def find_email(text) -> list:
