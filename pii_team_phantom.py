9b91b457199db534587bbaccaf0ac1ddc2a6ee42
Jordan Holeman <jholeman2001@gmail.com>
2022-10-23 20:06:52 -0400

    phone number

--- pii_team_phantom.py
+++ pii_team_phantom.py
@@ -4,6 +4,7 @@ import re
 def find_us_phone_number(text) -> list:
     """Finds all occurrences of a US phone number in a text string"""
     # match a 10 digit phone number with area code
+    
     return re.findall(r'\d{3}-\d{3}-\d{4}', text)
 
 
