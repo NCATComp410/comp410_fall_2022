23a338f6a1dc7448c73f5e5a3786ce6d9d5f25c1
uvmoore <56407203+uvmoore@users.noreply.github.com>
2022-10-23 20:43:20 -0400

    I added SSN check

--- pii_team_phantom.py
+++ pii_team_phantom.py
@@ -23,7 +23,7 @@ def find_amex(text) -> list:
 def find_us_ssn(text) -> list:
     """Finds all occurrences of a US social security number in a text string"""
     # match a 9 digit social security number
-    return []
+    return re.findall(r'\d{3}\-\d{2}\-\d{4}', text)
 
 
 def find_email(text) -> list:
