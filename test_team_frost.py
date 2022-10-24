7aff3a9240cca3bf181bbe55847ceb8a2a7ff26c
DougGajda <112100715+DougGajda@users.noreply.github.com>
2022-10-23 22:30:38 -0400

    Resubmission of Sprint 2 with added changes
    This is a resubmission with the requested changes being asked for.

--- test_team_frost.py
+++ test_team_frost.py
@@ -1,4 +1,6 @@
+from pickle import FALSE
 import unittest
+from unittest import result
 from pii_team_frost import *
 
 
@@ -20,6 +22,18 @@ class TeamFrostTests(unittest.TestCase):
         lst = ["123-45-6789"]
         message = " - US SSN Lists Dont Match"
         self.assertEqual(results_list, [lst], message)
+
+        # Test for multiple US SSN within a given sentance
+        results_list = find_us_ssn('My friends social security is 245-57-8359, and my other friends social security is 678-52-4878')
+        lst2 = ["245-57-8359", "678-52-4878"]
+        message = " - US SSN Lists Dont Match"
+        self.assertEqual(results_list,[lst2],message)
+
+        # Test for invalid case where SSN was not in right format
+        results_list = find_us_ssn('My Social Security is 23-56-4576')
+        message = " - Invalid SSN Format"
+        self.assertTrue(results_list, message)
+
 
     def test_find_email(self):
         results_list = find_email('My email address is jim.jones@jones.com')
