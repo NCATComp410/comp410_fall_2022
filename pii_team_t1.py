ef0b96014f9566208f89f953cbfefa74d32c749f
Mjackson22 <46692254+Mjackson22@users.noreply.github.com>
2022-10-25 19:50:42 -0400

    Update pii_team_t1.py

--- pii_team_t1.py
+++ pii_team_t1.py
@@ -16,7 +16,7 @@ def find_visa_mastercard(text) -> list:
 def find_amex(text) -> list:
     """Finds all occurrences of an amex number in a text string"""
     # match a 15 digit credit card number
-    return []
+    return re.findall(r'\d{4}-\d{6}-\d{5},' text)
 
 
 def find_us_ssn(text) -> list:
