2cbd7499703fa0975155fc869d28a0fa01331ede
adwilliams8 <59934444+adwilliams8@users.noreply.github.com>
2022-10-24 21:39:40 -0400

    Update test_team_t2.py

--- test_team_t2.py
+++ test_team_t2.py
@@ -22,10 +22,47 @@ class TeamFrostTests(unittest.TestCase):
     def test_find_email(self):
         results_list = find_email('My email address is jim.jones@jones.com')
         self.assertEqual(results_list, [])
-
+    
+    #testing alphabetical
     def test_find_instagram_handle(self):
-        results_list = find_instagram_handle('My instagram handle is @jimjones')
-        self.assertEqual(results_list, [])
+        results_list = find_instagram_handle('My instagram handle is @ariorwateva')
+        self.assertEqual(results_list[0], '@ariorwateva')
+   
+    #testing non case-sensitive
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is @Ariorwateva')
+        self.assertEqual(results_list[0], '@Ariorwateva')
+   
+    #testing non alphanumeric
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is @ariorwateva25')
+        self.assertEqual(results_list[0], '@ariorwateva25')
+        
+    #testing underscore
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is @ariorwateva_')
+        self.assertEqual(results_list[0], '@ariorwateva_')
+    
+    #testing '.'
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is @ari.orwateva')
+        self.assertEqual(results_list[0], '@ari.orwateva')
+        
+    #testing invalid case with no @
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is ariorwateva')
+        self.assertFalse(result_list)
+    
+    #testing invalid case with invalid character
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('My instagram handle is @arior!wateva')
+        self.assertFalse(result_list)
+    
+    #testing handle at the beginning of the string
+    def test_find_instagram_handle(self):
+        results_list = find_instagram_handle('@ariorwateva is my instagram handle')
+        self.assertEqual(results_list[0], '@ari.orwateva')
+    
 
 
 if __name__ == '__main__':
