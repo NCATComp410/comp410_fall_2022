5d1cb21a8e99dab3dc2daf1511f26d2e374227e4
Ericas14 <52893140+Ericas14@users.noreply.github.com>
2022-10-25 11:08:06 -0400

    Add regex for instagram handles

--- pii_team_phantom.py
+++ pii_team_phantom.py
@@ -36,4 +36,4 @@ def find_email(text) -> list:
 def find_instagram_handle(text) -> list:
     """Finds all occurrences of an instagram handle in a text string"""
     # match an instagram handle
-    return []
+    return re.findall(r'@[\w._]{1,30}',text)
