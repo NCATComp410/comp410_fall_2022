67c92ac7d29c8e2c9f140e3da8f6355da8f9f9c2
kalynmatth <54991933+kalynmatth@users.noreply.github.com>
2022-10-25 11:27:55 -0400

    Update test_team_t2.py

--- test_team_t2.py
+++ test_team_t2.py
@@ -2,7 +2,7 @@ import unittest
 from pii_team_t2 import *
 
 
-class TeamFrostTests(unittest.TestCase):
+class TeamT2Tests(unittest.TestCase):
     def test_find_us_phone_number(self):
         results_list = find_us_phone_number('My phone number is 123-456-7890')
         self.assertEqual(results_list, [])  # add assertion here
@@ -22,6 +22,26 @@ class TeamFrostTests(unittest.TestCase):
     def test_find_email(self):
         results_list = find_email('My email address is jim.jones@jones.com')
         self.assertEqual(results_list, [])
+        
+         results_list = find_email('My email address is janedoe45@mbc.com')
+        self.assertEqual(results_list[0], 'janedoe45@mbc.com')
+
+         #return results at the beginning of string
+         results_list = find_email('janedoe45@mbc.com is my email address')
+         self.assertEqual(results_list[0], 'janedoe45@mbc.com')
+
+         #return results with multiple addresses
+         results_list = find_email('My email address is janedoe45@mbc.com')
+         self.assertEqual(results_list[0], 'janedoe45@mbc.com ')
+         self.assertEqual(results_list[1], 'doejane45@mbc.com')
+
+         #return results for wrong formatting 
+         results_list = find_email('My email address is doejane45@mbc.com')
+         self.assertFalse(results_list)
+
+         #return results with unneccessary characters
+         results_list = find_email('My email address is janedoe45;.@mbc.com')
+         self.assertFalse(results_list)
 
     def test_find_instagram_handle(self):
         results_list = find_instagram_handle('My instagram handle is @jimjones')
