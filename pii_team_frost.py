df3e588f8b3cc88dc230e5c22370e801c642dfa1
ahjoyner <ahjoyner@aggies.ncat.edu>
2022-11-29 12:46:17 -0500

    Fixed find_amex

--- pii_team_frost.py
+++ pii_team_frost.py
@@ -28,7 +28,7 @@ def find_visa_mastercard(text):
 def find_amex(text):
     """Finds all occurrences of an amex number in a text string"""
     # match a 15 digit credit card number
-    return re.findall(r'^(34|37)\d{2}-\d{6}-\d{5}', text)
+    return re.findall(r'^3[4]|[7]\d{2}-\d{6}-\d{5}$', text)
 
 
 def find_us_ssn(text):
